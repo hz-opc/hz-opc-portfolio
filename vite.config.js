748a0844fb0307d79d049954399136ae9d2e18ec
HZ <hz@portfolio.local>
2026-06-11 15:43:19 +0800

    Fix: remove singlefile plugin and fix base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { viteSingleFile } from 'vite-plugin-singlefile'
+﻿import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-  plugins: [react(), viteSingleFile()],
-  base: '/hz-opc-portfolio/',
+  plugins: [react()],
+  base: "/",
 })
